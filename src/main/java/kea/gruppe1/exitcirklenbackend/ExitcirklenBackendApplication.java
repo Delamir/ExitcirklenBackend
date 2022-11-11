c5bd6845133d62957dced51d0b378c3977d7e58e
Delamir <td_christian@hotmail.com>
2022-11-11 11:43:28 +0100

    Second commit

---
+++ src/main/java/kea/gruppe1/exitcirklenbackend/ExitcirklenBackendApplication.java
@@ -0,0 +1,13 @@
+package kea.gruppe1.exitcirklenbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExitcirklenBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExitcirklenBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kea/gruppe1/exitcirklenbackend/ExitcirklenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.gruppe1.exitcirklenbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExitcirklenBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
