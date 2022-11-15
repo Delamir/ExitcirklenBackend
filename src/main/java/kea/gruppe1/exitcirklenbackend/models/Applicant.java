99b355fdc1c941d05f236332871d5efa9f95a0e9
Delamir <td_christian@hotmail.com>
2022-11-15 11:01:52 +0100

    Oprettet applicant model og enum til byer.

---
+++ src/main/java/kea/gruppe1/exitcirklenbackend/models/Applicant.java
@@ -0,0 +1,47 @@
+package kea.gruppe1.exitcirklenbackend.models;
+
+import lombok.Data;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Table(name = "applicants")
+@Entity
+public class Applicant {
+
+    @Id
+    @Column
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String age;
+
+    @Column
+    private String gender;
+
+    @Column
+    private String email;
+
+    @Column
+    private String phoneNumber;
+
+    @Column
+    private String city;
+
+    @Column
+    private int priority;
+
+    @Column
+    private boolean contactCall;
+
+    @Column
+    private boolean contactText;
+
+}
---
+++ src/main/java/kea/gruppe1/exitcirklenbackend/models/Cities.java
@@ -0,0 +1,11 @@
+package kea.gruppe1.exitcirklenbackend.models;
+
+public enum Cities {
+    KØBENHAVN,
+    HILLERØD,
+    KØGE,
+    ODENSE,
+    ÅRHUS,
+    ESBJERG,
+    AALBORG
+}
